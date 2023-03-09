a91e3e91bd77e6b11fdbc97260fabda76f020d33
_flyckks-dev <fernando.mieza@gmail.com>
2023-03-09 03:02:05 -0300

    Update animatedtext.cpp

--- src/client/animatedtext.cpp
+++ src/client/animatedtext.cpp
@@ -29,7 +29,7 @@
 
 AnimatedText::AnimatedText()
 {
-    m_cachedText.setFont(g_fonts.getFont("verdana-11px-rounded"));
+    m_cachedText.setFont(g_fonts.getFont("newfont-16px-roundedd"));
     m_cachedText.setAlign(Fw::AlignLeft);
 }
 
